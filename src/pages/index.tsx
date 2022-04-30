369b242151ead55a10dac20e4ae2c4836658ca71
田所大樹 <tadokoro@crewinc.io>
2022-05-01 08:18:51 +0900

    add-blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+import { MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticProps, NextPage } from "next";
 import { client } from "../libs/client";
+import Link from "next/link";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+export type Blog = {
+  title: string;
+  body: string;
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+type Props = MicroCMSListResponse<Blog>;
+
+const Home: NextPage<Props> = (props) => {
+  return (
+    <div>
+      <p>{`記事の総数：${props.totalCount}`}</p>
+      <ul>
+        {props.contents.map((content) => {
+          return (
+            <li key={content.id}>
+              <Link href={`/blog/${content.id}`}>
+                <a>{content.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
   const data = await client.getList({ endpoint: "blog" });
-  console.log(data);
   return {
-    props: {},
+    props: data,
   };
 };
 
